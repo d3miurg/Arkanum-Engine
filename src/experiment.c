eb379489ef6960d361aef66966158a56d1215576
d3miurg <50408380+d3miurg@users.noreply.github.com>
2025-02-23 12:13:52 +0300

    added raygui

--- src/experiment.c
+++ src/experiment.c
@@ -1,16 +1,28 @@
 #include "raylib.h"
 
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
 int main(void) {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Test");
-
     SetTargetFPS(60);
+
+    bool showMessageBox = 0;
 
     while(!WindowShouldClose()) {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
+            if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "Show Message")) showMessageBox = 1;
+
+            if (showMessageBox)
+            {
+                int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
+                    "Message Box", "Hi! This is a message!", "Nice;Cool");
+
+                if (result >= 0) showMessageBox = 0;
+            }
         EndDrawing();
     }
 
